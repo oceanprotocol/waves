dae638d16e2b130045c86c37fe7593ec46a12d7a
Gavrila Alexandru <alex.gavrila96@gmail.com>
2022-10-25 12:11:35 +0300

    refactor: audio player design

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,33 +1,41 @@
 import { usePlayerContext } from '@context/Player.context'
+import useWindowDimensions from '@hooks/useWindowDimensions'
 import React, { ReactElement } from 'react'
 import AudioPlayer from 'react-h5-audio-player'
 
-import s from './Player.module.css'
+import styles from './Player.module.css'
 
 export default function Player(): ReactElement {
   const { song, loading, close, songData } = usePlayerContext()
+  const { width } = useWindowDimensions()
 
   const player = (
-    <AudioPlayer
-      src={song}
-      // layout="horizontal"
-      header={
-        <div className={s.songDetails}>
-          <div>album: {songData?.album}</div>
-          <img
-            style={{ margin: 0 }}
-            width={60}
-            height={60}
-            src={songData?.coverPicture}
-            alt="cover"
-          />
-          <div>
-            <div>{songData?.artist}</div>
-            <div>{songData?.title}</div>
+    <div className={styles.playerWrapper}>
+      <img
+        style={{ margin: 0 }}
+        width={70}
+        height={70}
+        src={songData.coverPicture ?? '/placeholderImage.jpg'}
+        alt="cover"
+        className={styles.playerCover}
+      />
+      <AudioPlayer
+        src={song}
+        layout={width > 600 ? 'horizontal' : 'stacked'}
+        autoPlay
+        showJumpControls={false}
+        className={styles.audioPlayer}
+        header={
+          <div className={styles.songDetails}>
+            {/* <div>album: {songData?.album}</div> */}
+            <div>
+              <h4 className={styles.songTitle}>{songData?.title}</h4>
+              <div>{songData?.artist}</div>
+            </div>
           </div>
-        </div>
-      }
-    />
+        }
+      />
+    </div>
   )
 
   if (!song) {
@@ -36,16 +44,16 @@ export default function Player(): ReactElement {
 
   if (loading) {
     return (
-      <div className={s.loadingRoot}>
-        <div className={s.loadingPlayer}>{player}</div>
+      <div className={styles.loadingRoot}>
+        <div className={styles.loadingPlayer}>{player}</div>
       </div>
     )
   }
 
   return (
-    <div className={s.root}>
-      <div className={s.closeContainer}>
-        <button className={s.closeButton} onClick={close}>
+    <div className={styles.root}>
+      <div className={styles.closeContainer}>
+        <button className={styles.closeButton} onClick={close}>
           ✖
         </button>
       </div>
